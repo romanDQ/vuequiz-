7531f760e5792e7017bc1b65305ad247b1a3e124
romandq <romandq@gmail.com>
2020-03-16 10:14:28 +0100

    initial

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="question-box-container">
+    <b-jumbotron>
+      <template v-slot:lead>{{currentQuestion.question}}</template>
+
+      <hr class="my-4" />
+
+      <b-list-group>
+        <b-list-group-item
+        v-for="(answer, index) in answers"
+        :key="index"
+        >{{answer}}</b-list-group-item>
+      </b-list-group>
+    
+
+      <p  :key="index">{{answer}}</p>
+
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button @click="next" variant="success" href="#">Next</b-button>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    currentQuestion: Object,
+    next: Function
+  },
+  computed: {
+    answers() {
+      let answers = [...this.currentQuestion.incorrect_answers];
+      answers.push(this.currentQuestion.correct_answer);
+      return answers;
+    }
+  },
+
+  mounted() {
+    console.log(this.currentQuestion);
+  }
+};
+</script>
